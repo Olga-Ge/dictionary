41f0e126495e1d76385706780c8f8e03083702c1
user <olga.gezha@gmail.com>
2020-03-09 23:31:14 +0100

    1

--- translator.py
+++ translator.py
@@ -1,6 +1,12 @@
 import rus_dictionary
 print (rus_dictionary.d)
+text = "cat"
 
+translate = ""
+words = text.split()
+for w in words:
+    translate = translate + " "+ rus_dictionary.d(w)
+print(translate)
 
 
 
